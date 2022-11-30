05dd3e769be9ac26798b0e4b0b8e552354f7bef8
AtsuneMogi <maomudunyin@gmail.com>
2022-11-30 22:30:33 +0900

    in progress

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,19 @@
 #define SPEED 150
 #define TURN_SPEED 100
 
-#define speedPinR 9   //  Front Wheel PWM pin connect Model-Y M_B ENA 
-#define RightMotorDirPin1  22    //Front Right Motor direction pin 1 to Model-Y M_B IN1  (K1)
-#define RightMotorDirPin2  24   //Front Right Motor direction pin 2 to Model-Y M_B IN2   (K1)                                 
-#define LeftMotorDirPin1  26    //Front Left Motor direction pin 1 to Model-Y M_B IN3 (K3)
-#define LeftMotorDirPin2  28   //Front Left Motor direction pin 2 to Model-Y M_B IN4 (K3)
-#define speedPinL 10   //  Front Wheel PWM pin connect Model-Y M_B ENB
+#define speedPinR 9 // Front Wheel PWM pin connect Model-Y M_B ENA 
+#define RightMotorDirPin1 22 // Front Right Motor direction pin 1 to Model-Y M_B IN1 (K1)
+#define RightMotorDirPin2 24 // Front Right Motor direction pin 2 to Model-Y M_B IN2 (K1)                                 
+#define LeftMotorDirPin1 26 // Front Left Motor direction pin 1 to Model-Y M_B IN3 (K3)
+#define LeftMotorDirPin2 28 // Front Left Motor direction pin 2 to Model-Y M_B IN4 (K3)
+#define speedPinL 10 // Front Wheel PWM pin connect Model-Y M_B ENB
 
-#define speedPinRB 11   //  Rear Wheel PWM pin connect Left Model-Y M_A ENA 
-#define RightMotorDirPin1B  5    //Rear Right Motor direction pin 1 to Model-Y M_A IN1 ( K1)
-#define RightMotorDirPin2B 6    //Rear Right Motor direction pin 2 to Model-Y M_A IN2 ( K1) 
-#define LeftMotorDirPin1B 7    //Rear Left Motor direction pin 1 to Model-Y M_A IN3  (K3)
-#define LeftMotorDirPin2B 8  //Rear Left Motor direction pin 2 to Model-Y M_A IN4 (K3)
-#define speedPinLB 12    //  Rear Wheel PWM pin connect Model-Y M_A ENB
+#define speedPinRB 11 // Rear Wheel PWM pin connect Left Model-Y M_A ENA 
+#define RightMotorDirPin1B 5 // Rear Right Motor direction pin 1 to Model-Y M_A IN1 (K1)
+#define RightMotorDirPin2B 6 // Rear Right Motor direction pin 2 to Model-Y M_A IN2 (K1) 
+#define LeftMotorDirPin1B 7 // Rear Left Motor direction pin 1 to Model-Y M_A IN3 (K3)
+#define LeftMotorDirPin2B 8 // Rear Left Motor direction pin 2 to Model-Y M_A IN4 (K3)
+#define speedPinLB 12 // Rear Wheel PWM pin connect Model-Y M_A ENB
 
 char ssid[] = "M5StickC-Plus-Controller";
 char pass[] = "controller"; 
@@ -225,8 +225,7 @@ void setup() {
 
 void loop() {
     Serial.println("Unko!!!!!!"); delay(2000);
-    /*int packetSize = Udp.parsePacket();
-    
+    /*    
     switch (buf[0]) {
         case 'a':
             forward(SPEED);
